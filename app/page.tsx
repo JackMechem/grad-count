c6d88117cfa4e47daa6465599e187dac86b355be
Jack Mechem <mechemjack@gmail.com>
2024-01-16 21:11:03 -0800

    milliseconds and graduated message

--- app/page.tsx
+++ app/page.tsx
@@ -6,32 +6,46 @@ import { useEffect, useState } from "react";
 export default function Home() {
   const currDate = moment(new Date());
   const gradDate = moment(new Date(2024, 4, 21, 18, 0, 0));
-  const diffrenceDate = moment.duration(gradDate.diff(currDate));
 
-  const [seconds, setSeconds]: any = useState(null);
+  const [countdown, setCountdown]: any = useState(null);
+  const [graduated, setGraduated] = useState(false);
 
   useEffect(() => {
     const currDates = moment(new Date());
-    const diffrenceDates = moment.duration(gradDate.diff(currDates));
+    const diffrenceDate = moment.duration(gradDate.diff(currDates));
     setTimeout(() => {
-      setSeconds(diffrenceDates.seconds());
-    }, 1000);
+      setCountdown(
+        ~~diffrenceDate.asDays() +
+          ":" +
+          diffrenceDate.hours() +
+          ":" +
+          diffrenceDate.minutes() +
+          ":" +
+          diffrenceDate.seconds() +
+          ":" +
+          diffrenceDate.milliseconds(),
+      );
+
+      if (currDate >= gradDate) {
+        setGraduated(true);
+      } else {
+        setGraduated(false);
+      }
+    }, 10);
   });
 
   return (
-    <main className="w-screen h-screen bg-black text-white font-semibold text-[5vw] text-center flex items-center justify-center">
-      <div>
-        <p>Graduation</p>
-        <span>
-          {~~diffrenceDate.asDays() +
-            ":" +
-            ~~diffrenceDate.hours() +
-            ":" +
-            ~~diffrenceDate.minutes() +
-            ":"}
-        </span>
-        <span>{seconds}</span>
-      </div>
+    <main className="w-full h-full bg-black text-white font-semibold text-[5vw] text-center flex items-center justify-center">
+      {graduated ? (
+        <div className="bg-slate-900 p-10 rounded-2xl border border-slate-700 shadow-slate-900 shadow-lg text-center">
+          <p>NO MORE BOWEN</p>
+        </div>
+      ) : (
+        <div className="bg-slate-900 p-10 rounded-2xl border border-slate-700 shadow-slate-900 shadow-lg text-center">
+          <p>Graduation</p>
+          <span>{countdown}</span>
+        </div>
+      )}
     </main>
   );
 }
